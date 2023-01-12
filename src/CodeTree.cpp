5e7e742a61e0713442588892f297de01764dea70
Philippe Gras <philippe.gras@cern.ch>
2023-01-12 17:12:14 +0100

    bug fix and message improvement * bug fix: function using std type natively supported by CxxWrap like std::string were not wrapped if the type contained qualifiers like const or &. Fixed. * diagnostic message from clang are now displayed. * clang is run in verbose mode (-v) if wrapit verbosity level is set to 2 or more.

--- src/CodeTree.cpp
+++ src/CodeTree.cpp
@@ -1166,11 +1166,6 @@ CodeTree::register_type(const CXType& type){
     "std::vector<void*>",
   };
 
-  if(std::find(natively_supported.begin(), natively_supported.end(), type_name)
-     != natively_supported.end()){
-    return true;
-  }
-
   bool usable;
   std::vector<CXCursor> defs;
   std::tie(usable, defs) = find_type_definition(type);
@@ -1202,7 +1197,13 @@ CodeTree::register_type(const CXType& type){
       // abort();
     }
 
-    if(type0.kind == CXType_Record){
+  if(std::find(natively_supported.begin(), natively_supported.end(), type0_name)
+     != natively_supported.end()){
+    return true;
+  }
+
+    
+  if(type0.kind == CXType_Record){
 
       bool found = false;
 
@@ -1884,6 +1885,11 @@ CodeTree::parse(std::ofstream& header_file, const std::filesystem::path& header_
 
   std::vector<const char*> opts(opts_.size());
   for(unsigned i = 0; i < opts_.size(); ++i) opts[i] = opts_[i].c_str();
+
+  if(verbose > 1){
+    //Enable clang verbose option
+    opts.push_back("-v");
+  }
 
   if(verbose > 0){
     std::cerr << "Clang options:\n";
