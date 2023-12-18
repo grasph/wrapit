grasph/wrapit

commit de83cd73145af646482ef1a751b14000a0dd2261

. Added vetoed_copy_ctor_classes configurable to allow explicit veto of copy constructor of selected classes. . Fixed auto_veto for methods depending on a template parameter of its class. auto_veto is now enabled in TestTemplate2 . prefix operator * is now mapped to [] (getindex()))

src/CodeTree.cpp
@@ -232,6 +232,9 @@ CodeTree::generate_cxx_for_type(std::ostream& o,
   //if true, fake type used to hold global functions
   bool notype = t.type_name.size() == 0;
 
+  bool no_copy_ctor = find(copy_ctor_to_veto_.begin(),
+                           copy_ctor_to_veto_.end(), t.type_name) != copy_ctor_to_veto_.end();
+  
   if(!notype){
     o << "\nnamespace jlcxx {\n";
     //generate code that disables mirrored type
@@ -263,9 +266,15 @@ CodeTree::generate_cxx_for_type(std::ostream& o,
       indent(o,1) << "};\n\n";
       indent(o, 1) << "template<" << param_list1 << "> struct IsMirroredType<" << t.type_name << "<" << param_list2 << ">> : std::false_type { };\n";
       indent(o, 1) << "template<" << param_list1 << "> struct DefaultConstructible<" << t.type_name << "<" << param_list2 << ">> : std::false_type { };\n";
+      if(no_copy_ctor){
+        indent(o, 1) << "template<" << param_list1 << "> struct CopyConstructible<" << t.type_name << "<" << param_list2 << ">> : std::false_type { };\n";
+      }
     } else{
       indent(o, 1) << "template<> struct IsMirroredType<" << t.type_name << "> : std::false_type { };\n";
       indent(o, 1) << "template<> struct DefaultConstructible<" << t.type_name << "> : std::false_type { };\n";
+      if(no_copy_ctor){
+        indent(o, 1) << "template<> struct CopyConstructible<" << t.type_name << "> : std::false_type { };\n";
+      }
     }
 
     //generate inheritance mapping
@@ -1395,17 +1404,32 @@ CodeTree::register_type(const CXType& type){
 
 std::tuple<std::vector<CXType>, int>
 CodeTree::visit_function_arg_and_return_types(CXCursor cursor){
-
+  
   const auto& method_type = clang_getCursorType(cursor);
   const auto return_type = clang_getResultType(method_type);
 
+  TypeRcd* pTypeRcd = find_class_of_method(cursor);
+  //TODO: find a more robust way than comparing identifier  names.
+  auto is_class_param = [pTypeRcd](CXType type){
+    if(pTypeRcd==nullptr) return true;
+    auto type_name = remove_cv(str(clang_getTypeSpelling(base_type(type))));
+    std::cerr << "Looking for type_name " << type_name << "\n";
+    auto params = pTypeRcd->template_parameters;
+    return std::find(params.begin(), params.end(), type_name) != params.end();
+  };
+  
   std::vector<CXType> missing_types;
 
   if(return_type.kind != CXType_Void){
-    bool rc = register_type(return_type);
-    if(!rc) missing_types.push_back(return_type);
+    if(verbose > 3) std::cerr << cursor << ", return type: " << return_type << "\n";
+    if(is_class_param(return_type)){
+      if(verbose > 3) std::cerr << cursor << " identified as a parameter of the holding class\n";
+    } else{
+      bool rc = register_type(return_type);
+        if(!rc) missing_types.push_back(return_type);
+    }
   }
-
+  
   for(int i = 0; i < clang_getNumArgTypes(method_type); ++i){
     auto argtype = clang_getArgType(method_type, i);
     if(verbose > 3) std::cerr << cursor << ", arg " << (i+1) << " type: " << argtype << "\n";
@@ -1414,8 +1438,12 @@ CodeTree::visit_function_arg_and_return_types(CXCursor cursor){
       bool rc = register_type(eltype);
       if(!rc) missing_types.push_back(argtype);
     } else if(argtype.kind != CXType_Void){
-      bool rc = register_type(argtype);
-      if(!rc) missing_types.push_back(argtype);
+      if(is_class_param(argtype)){
+        if(verbose > 3) std::cerr << cursor << " identified as a parameter of the holding class\n";
+      } else{
+        bool rc = register_type(argtype);
+        if(!rc) missing_types.push_back(argtype);
+      }
     }
   }
 

test/TestTemplate2/A.h
@@ -1,13 +1,13 @@
-template<typename T>
+template<typename S>
 class A {
 public: 
   A(): a(0){}
 
-  T getval(){ return a;}
+  S getval(){ return a;}
   
-  void setval(const T& val){ a = val; }
+  void setval(const S& val){ a = val; }
 
-  T a;
+  S a;
 };
 
 template class A<int>;

src/CodeTree.h
@@ -204,6 +204,11 @@ namespace codetree{
       finalizers_to_veto_ = val;
     }
 
+    void vetoed_copy_ctor_classes(const std::vector<std::string>& val){
+      for(const auto& k: val) copy_ctor_to_veto_.insert(k);
+    }
+
+    
     void build_cmd(const std::string& val){ build_cmd_ = val; }
 
     void enableTestBuild(bool val){ test_build_ = val; }
@@ -423,6 +428,9 @@ namespace codetree{
     //List of classes whose finalizer has been disabled
     std::set<std::string> vetoed_finalizers_;
 
+    //List of classes whose wrapping of copy constructor has been disabled by configuration
+    std::set<std::string> copy_ctor_to_veto_;
+    
     bool visiting_a_templated_class_;
 
     std::vector<std::string> include_dirs_;

src/FunctionWrapper.cpp
@@ -480,6 +480,10 @@ FunctionWrapper::FunctionWrapper(const MethodRcd& method, const TypeRcd* pTypeRc
     override_base_ = false;
   }
 
+  if(name_jl_suffix == "*" && clang_getNumArgTypes(method_type)){
+       name_jl_suffix = "getindex"; //Deferencing operator, *x -> x[]
+  }
+
   std::vector<std::pair<std::string, std::string>> op_map = {
     {"()", "paren"},
     {"+=", "add!"},
@@ -492,6 +496,7 @@ FunctionWrapper::FunctionWrapper(const MethodRcd& method, const TypeRcd* pTypeRc
     {"^=", "bwxor!"},
     {"|=", "bwor!"},
     {"&=", "bwand!"},
+    {"*", "getindex"} //Deferencing of x written as x[]
   };
 
   for(const auto& m: op_map){

src/main.cpp
@@ -129,6 +129,7 @@ int main(int argc, char* argv[]){
 
     auto inheritances = read_vstring("inheritances");
     auto vetoed_finalizer_classes  = read_vstring("vetoed_finalizer_classes");
+    auto vetoed_copy_ctor_classes  = read_vstring("vetoed_copy_ctor_classes");
 
     auto module_name = toml_config["module_name"].value_or(std::string("CxxLib"));
     auto out_export_jl_fname = toml_config["export_jl_fname"].value_or(std::string());
@@ -242,6 +243,7 @@ int main(int argc, char* argv[]){
     tree.build_cmd(std::string(build_cmd));
     tree.inheritances(inheritances);
     tree.vetoed_finalizer_classes(vetoed_finalizer_classes);
+    tree.vetoed_copy_ctor_classes(vetoed_copy_ctor_classes);
     tree.accessor_generation_enabled(fields_and_variables);
 
     tree.set_n_classes_per_file(n_classes_per_file);

test/TestTemplate2/TestTemplate2.wit
@@ -4,7 +4,7 @@ input = [ "A.h" ]
 		  
 include_dirs = [ "." ]
 
-auto_veto = false
+auto_veto = true
 
 cxx-std = "c++17"
 

commit 85276a28d1d1d0f7719d7d798534bb265f462606

Bug fix: automatic wrapping of types used by functions was no more effective for *global* functions.
- The bug was introduced by commit de83cd7 and release 1.1.0.
- Added unit test for this feature, TestAutoAdd.

src/CodeTree.cpp
@@ -1501,7 +1501,7 @@ CodeTree::visit_function_arg_and_return_types(CXCursor cursor){
   //TODO: find a more robust way than comparing identifier  names.
   auto is_class_param = [cursor, this](CXType type){
     TypeRcd* pTypeRcd = find_class_of_method(cursor);
-    if(pTypeRcd==nullptr) return true;
+    if(pTypeRcd==nullptr) return false;
     auto type_name = remove_cv(str(clang_getTypeSpelling(base_type_(type))));
     auto params = pTypeRcd->template_parameters;
     return std::find(params.begin(), params.end(), type_name) != params.end();

test/TestAutoAdd/.gdb_history
@@ -1,0 +1,256 @@
+l
+l -10
+l -10
+p type0
+p type
+br 975
+dis 1 
+dis 2
+r
+s
+s
+n
+p type
+n
+p type1
+n
+n
+n
+p type0.kind
+l
+n
+p type0
+fin
+s
+n
+s
+p CT.kind
+n
+n
+n
+n
+n
+n
+n
+n
+n
+n
+n
+p CXCursor_NoDeclFound
+fin
+fin
+fin
+fin
+n
+p usable
+p defs
+br register_type(CXType const&)
+r
+br register_type(CXType const&)
+r
+n
+n
+p type_name
+n
+n
+s
+n
+p type0
+p type
+br 975
+dis 1
+r
+s
+s
+n
+n
+n
+n
+n
+n
+p type0.kind
+fin
+s
+n
+s
+p type.kind 
+p CXType_LastBuiltin
+p (int) CXType_LastBuiltin
+p (int)type.kind
+p (int)CXType_FunctionProto
+l
+s
+l
+s
+n
+n
+n
+n
+p u
+fin
+fin
+n
+r
+p type
+n
+p type0
+r
+s
+s
+n
+n
+p type1
+n
+p type0
+l
+l
+until 93
+p type0.kind
+fin
+s
+n
+p type
+s
+s
+s
+n
+n
+n
+n
+n
+p u
+n
+n
+n
+n
+r
+br 975
+br register_type(CXType const&)
+r
+br register_type(CXType const&)
+r
+n
+n
+p type_name
+n
+n
+s
+s
+p type
+s
+n
+p type
+p typ0
+p type0
+n
+n
+l
+l
+p type0
+n
+n
+p type0.kind
+l
+n
+p type0
+fin
+s
+l
+p (int)type.kind
+p type.kind 
+p (int)CXType_LastBuiltin
+l
+l
+l -10
+l -10
+bt
+p type
+s
+fin
+p type
+p clang_getTypeDeclaration(type)
+r
+r
+br register_type(CXType const&)
+r
+n
+n
+p type_name
+fin
+fin
+s
+fin
+fin
+r
+dn
+n
+l
+l
+until 1098
+s
+s
+s
+n
+n
+n
+n
+n
+n
+fin
+s
+n
+p type
+fin
+l
+fin
+n
+n
+n
+n
+p type0
+s
+n
+n
+n
+n
+n
+n
+p devl
+p devl
+p decl
+l
+n
+n
+n
+p verbose
+n
+n
+n
+p def
+n
+p decl
+r
+bt
+br FunctionWrapper::signature
+l
+r
+l
+l
+display pTypeRcd
+n
+n
+r
+r
+bt
+r
+bt
+r
+bt
+frame 3
+p t
+pt t.type_name
+l 322
+p o
+bt
+r
+q

test/TestAutoAdd/A.h
@@ -1,0 +1,13 @@
+#ifndef A_H
+#define A_H
+
+#include "B.h"
+
+B2 f(const B1& b1){ return B2(b1); }
+
+struct A{
+  B4 f(const B3& b3){ return B4(b3); }
+};
+
+
+#endif //A_H not defined

test/TestAutoAdd/B.h
@@ -1,0 +1,19 @@
+struct B1{
+  B1(): i_(0){}
+  int i_;
+};
+
+struct B2{
+  B2(const B1& b1): b1_(b1){}
+  B1 b1_;
+};
+
+struct B3{
+  B3(): i_(0){}
+  int i_;
+};
+
+struct B4{
+  B4(const B3& b3): b3_(b3){}
+  B3 b3_;
+};

test/TestAutoAdd/Makefile
@@ -1,0 +1,8 @@
+#CXXFLAGS += -DVERBOSE_IMPORT #To enable the verbose mode of the libray loading
+#CXXFLAGS += -Wall -O0 -g     #To compile with debugger infomation
+
+MODULE_NAME=TestAutoAdd
+
+-include ../make.rules
+
+WRAPIT_VERBOSITY=1

test/TestAutoAdd/TestAutoAdd.wit
@@ -1,0 +1,17 @@
+module_name         = "TestAutoAdd"
+
+include_dirs        = [ "." ]
+
+input               = [ "A.h" ]
+
+#extra_input         = [ "cstdef" ]
+
+cxx-std             = "c++17"
+
+export  = "all"
+
+auto_veto = true
+
+# all generated code in a single file:
+n_classes_per_file = 0
+

test/TestAutoAdd/runTestAutoAdd.jl
@@ -1,0 +1,13 @@
+using Test
+
+@testset "Automatic type wrapping" begin
+    using TestAutoAdd
+
+    b1 = TestAutoAdd.B1()
+    @test isa(TestAutoAdd.f(b1), TestAutoAdd.B2)
+
+    a = TestAutoAdd.A()
+    b3 = TestAutoAdd.B3()
+    @test isa(f(a, b3), TestAutoAdd.B4)
+end
+

test/TestAutoAdd/setup.sh
@@ -1,0 +1,9 @@
+if [ X`uname`  = XDarwin ]; then
+  linker=DYLD
+else
+  linker=LD
+fi
+
+export JULIA_LOAD_PATH="@:@v#.#:@stdlib:`pwd`/TestAutoAdd/src"
+export ${linker}_LIBRARY_PATH="`pwd`/libTestAutoAdd"
+