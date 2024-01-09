bb0dcd053debc143f78c88f0a5eb1a7e83d58c20
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-01-09 15:38:14 +0100

    Do not need to pre-make the build area
    CMake does this for us

--- examples/ex001-HelloWorld/build.jl
+++ examples/ex001-HelloWorld/build.jl
@@ -5,10 +5,6 @@ cxxwrap_prefix = CxxWrap.prefix_path()
 
 build_location = "build"
 
-if !isdir(build_location)
-    mkdir(build_location)
-end
-
 try
     run(`cmake -S . -B $build_location -DCMAKE_PREFIX_PATH=$cxxwrap_prefix`)
     run(`cmake --build $build_location`)
