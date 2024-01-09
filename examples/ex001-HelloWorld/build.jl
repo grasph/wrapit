9151d4d1477fcb0fd874c7ca4ee0470ced968376
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-01-09 15:35:31 +0100

    Better fix for build directory
    Just avoid changing directory at all in the Julia script

--- examples/ex001-HelloWorld/build.jl
+++ examples/ex001-HelloWorld/build.jl
@@ -3,20 +3,16 @@
 using CxxWrap
 cxxwrap_prefix = CxxWrap.prefix_path()
 
-original_wd = pwd()
 build_location = "build"
 
 if !isdir(build_location)
     mkdir(build_location)
 end
-cd(build_location)
 
 try
-    run(`cmake -S .. -B . -DCMAKE_PREFIX_PATH=$cxxwrap_prefix`)
-    run(`cmake --build .`)
-    cd(original_wd)
+    run(`cmake -S . -B $build_location -DCMAKE_PREFIX_PATH=$cxxwrap_prefix`)
+    run(`cmake --build $build_location`)
     true
 catch
-    cd(original_wd)
     false
 end
