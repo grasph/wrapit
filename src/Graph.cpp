b415a5bfd471ff2e225eec3a7497b0354e49617d
Philippe Gras <philippe.gras@cern.ch>
2024-01-11 12:05:51 +0100

    removed an uncessary #include

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,49 +1,5 @@
-#include <bits/stdc++.h>
 using namespace std;
 #include "Graph.h"
-//class Graph {
-//public:
-//  Graph(): cyclic_(false), sorted_(false){}
-//
-//  /** Detects if the index ordering graph contains a cycle.
-//   * Returns true if a cycle was found, false otherwise.
-//   */
-//  bool isCyclic();
-//
-//  /** Specify ordering of two indices i and j,
-//   * more precisely that i preceeds j, i ≺ j.
-//   */
-//  void preceeds(unsigned i, unsigned j);
-//
-//  /** Returns the index in an order respecting the contraints
-//   * added with calls to the preceeds() function:
-//   *
-//   *  x_0 ≺ x_1 ≺ x_2 ≺ …
-//   *
-//   * cycleDetected is set to true if the dependency graph
-//   * contains a cycle e.g. 1 ≺ 2 ≺ 3 ≺ 5 …, 3 ≺ 1.
-//   */
-//  std::vector<unsigned> sortedIndices();
-//
-//private:
-//  void initScan();
-//
-//  void localSort(unsigned vertex);
-//
-//  void sort();
-//
-//  /* Edges of the graph: vector element at index i contains
-//   * the list of nodes that preceeds i
-//   */
-//  std::vector<list<unsigned>> edges_;
-//
-//  std::vector<bool> visited_;
-//  std::vector<bool> cyclicmarks_;
-//  std::vector<unsigned> sortednodes_;
-//  bool cyclic_;
-//  bool sorted_;
-//};
-
 void Graph::extend(unsigned n){
   if(n > edges_.size()){
     edges_.resize(n);
