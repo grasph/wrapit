c2888a935d78b2c90039e6bd5197b16cef3a455b
JamesWrigley <james@puiterwijk.org>
2024-06-11 14:19:50 +0200

    Add support for CxxWrap 0.16

--- src/cxxwrap_version.cpp
+++ src/cxxwrap_version.cpp
@@ -5,10 +5,11 @@
 //v0.10.0 (not used by any CxxWrap release),
 //v0.11.0 (CxxWrap v0.14.0),
 //v0.12.0 (CxxWrap v0.15.0)
+//v0.13.0 (not breaking, see https://github.com/JuliaInterop/CxxWrap.jl/releases/tag/v0.16.0)
 std::tuple<long, long> version_libcxxwrap_bounds(long cxxwrap_version){
   if(cxxwrap_version < cxxwrap_v0_15){
     return std::tuple<long, long>(11*1000, 12*1000);
   } else{
-    return std::tuple<long, long>(12*1000, 13*1000);
+    return std::tuple<long, long>(12*1000, 14*1000);
   }
 }
