488778264ea2e8256a02a70cac13979699c0ba04
JamesWrigley <james@puiterwijk.org>
2024-06-11 17:42:13 +0200

    Use the git commit hash as the version if not on a tag

--- version.cmake
+++ version.cmake
@@ -1,7 +1,14 @@
 execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --dirty --match "v*"
                 WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-                OUTPUT_VARIABLE GIT_TAG
+                OUTPUT_VARIABLE GIT_REF
                 OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
 
-configure_file(${SRC} ${DST} @ONLY)
+# If we're not on a tag, use the commit hash
+if(NOT GIT_REF)
+  execute_process(COMMAND ${GIT_EXECUTABLE} describe --always --dirty
+                  WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                  OUTPUT_VARIABLE GIT_REF
+                  OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+endif()
 
+configure_file(${SRC} ${DST} @ONLY)
