8c0bf3ec7a3f35116b96d948ada2f70475a0c97c
James Wrigley <JamesWrigley@users.noreply.github.com>
2024-06-12 16:35:42 +0200

    Add support for CxxWrap 0.16 (#55)

--- src/cxxwrap_version.h
+++ src/cxxwrap_version.h
@@ -1,9 +1,9 @@
 #include <tuple>
 
 //Default value for the cxxwrap_version config parameter
-static const char* default_cxxwrap_version = "0.15.0";
+static const char* default_cxxwrap_version = "0.16.0";
 
-static const char* supported_cxxwrap_versions = "0.14.x, 0.15.x";
+static const char* supported_cxxwrap_versions = "0.14.x, 0.15.x, 0.16.x";
 
 static int cxxwrap_v0_15 = 15 * 1000; //v0.15
 
